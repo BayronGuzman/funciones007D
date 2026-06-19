ec10738f72841aca3efe0bb72a978de328bf69c9
BayronGuzman <bay.guzman@duocuc.cl>
2026-06-19 19:28:42 -0400

    Create Prueba_4-Resevas_Hotel.py

---
+++ Prueba_4-Resevas_Hotel.py
@@ -0,0 +1,70 @@
+def mostrar_menu():
+    print("=====MENU PRINCIPAL=====")
+    print("1.- Agregar Reserva")
+    print("2.- Buscar Reserva")
+    print("3.- Eliminar Reserva")
+    print("4.- Confirmar Reservas")
+    print("5.- Mostrar Reservas")
+    print("6.- Salir")
+    
+def ingresar_opcion():
+    while True:
+        try:
+            opcion = int(input("Ingrese una opción (1 - 6): "))
+            if opcion < 1 or opcion > 6:
+                print("El número de la opción debe estar entre el 1 y el 6.")
+            else:
+                break
+        except ValueError:
+            print("Debe ingresar un número no un caracter")
+    
+    return opcion    
+
+def validar_nombre(name):
+    return name.strip() != ""
+
+def validar_numero_habitacion(numero_habitacion):
+    return numero_habitacion.isdigit() and int(numero_habitacion) >= 1 and int(numero_habitacion) <= 200
+
+def validar_cantidad_noches(cantidad_noches):
+    return cantidad_noches.isdigit() and int(cantidad_noches) > 0
+    
+
+def agregar_reserva(lista):
+    nombre = input("Ingrese el nombre del huesped: ")
+    correcto = validar_nombre(nombre)
+    if not correcto:
+        print("El nombre no puede estar vacío.")
+        return
+    
+    habitacion = input("Ingrese el número de la habitación: ")
+    correcto = validar_numero_habitacion(habitacion)
+    if not correcto:
+        print("El número de habitación debe ser un entero del 1 al 200.")
+        return
+            
+    noches = input("Ingrese la cantidad de noches: ")
+    correcto = validar_cantidad_noches(noches)
+    if not correcto:
+        print("La cantidad de noches debe ser un número entero positivo")
+        return
+        
+    reserva = {
+        "huesped": nombre.strip(),
+        "habitacion": int(habitacion),
+        "noches": int(noches),
+        "confirmada": False
+    }
+    
+    lista.append(reserva)
+    print("Reserva agregada correctamente")
+    
+    
+lista_reservas = []    
+op = 0
+while op != 6:
+    mostrar_menu()
+    op = ingresar_opcion()
+    
+    if op == 1:
+        agregar_reserva(lista_reservas)
